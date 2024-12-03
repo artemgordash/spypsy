f8db4a41fd278a8a3f8bee154b36a6b3129af2a6
Gordash Artem <artenngordas@gmail.com>
2024-12-03 20:34:15 +0200

    store requests and responses from devtools inside UI state

--- entrypoints/inspector/pages/home.tsx
+++ entrypoints/inspector/pages/home.tsx
@@ -1,3 +1,50 @@
+import { Box, Option, Select, Sheet, Stack, Typography } from '@mui/joy';
+import { ProtocolMapping } from 'devtools-protocol/types/protocol-mapping';
+
+type TabData = {
+  host: string;
+  requests: ProtocolMapping.Events['Network.requestWillBeSent'][0][];
+  responses: ProtocolMapping.Events['Network.responseReceived'][0][];
+  storage: any[];
+};
+
 export function HomePage() {
-  return <h1>Home</h1>;
+  const tabId = Number(new URLSearchParams(window.location.search).get('id'));
+  const [tabData, setTabData] = useState<TabData>({
+    host: '',
+    requests: [],
+    responses: [],
+    storage: [],
+  });
+
+  console.log(tabData);
+
+  useEffect(() => {
+    browser.debugger.onEvent.addListener((message, method, params) => {
+      if (method === 'Network.requestWillBeSent') {
+        const request =
+          params as ProtocolMapping.Events['Network.requestWillBeSent'][0];
+        setTabData((prev) => {
+          return {
+            ...prev,
+            requests: [...prev.requests, request],
+          };
+        });
+      }
+
+      if (method === 'Network.responseReceived') {
+        const response =
+          params as ProtocolMapping.Events['Network.responseReceived'][0];
+
+        setTabData((prev) => {
+          return {
+            ...prev,
+            responses: [...prev.responses, response],
+          };
+        });
+      }
+    });
+  }, []);
+
+  return <Box>Hello world{tabId}</Box>;
 }
